0df9b5f790acd40c120979015173b55880691c5e
Parker Swanson <pkswanson1@outlook.com>
2019-12-03 11:02:58 -0500

    Implemented some basic deck functions

--- deck.py
+++ deck.py
@@ -1,5 +1,6 @@
 #Will have the deck class (more or less lists of cards, but with some other implemented fields
 import cards
+import random
 
 """
 deck class
@@ -17,7 +18,7 @@ class Deck:
         self.num_starters = 0
         self. num_villains = 0
         self.num_weaknesses = 0
-        self.contents = []
+        self.contents = [] #cards left in deck
         #add the cards in card_list to self.contents
         for card in card_list:
             self.add_card(card)
@@ -42,16 +43,27 @@ class Deck:
 
     #Shuffles the deck
     def shuffle(self):
-        #TODO
-        return 0
+        #random's shuffle function does just this (thank you Python)
+        random.shuffle(self.contents)
 
     #draws card from deck and returns it
     def draw(self):
-        #TODO
-        return 0
+        card = self.pop(0)
+        if card.get_type() == "Power":
+            self.num_super_powers -= 1
+        elif card.get_type() == "Hero":
+            self.num_heroes -= 1
+        elif card.get_type() == "Equipment":
+            self.num_equipment -= 1
+        elif card.get_type() == "Starter":
+            self.num_starters -= 1
+        elif card.get_type() == "Villain":
+            self.num_villains -= 1
+        elif card.get_type() == "Weakness":
+            self.num_weaknesses -= 1
+        return
 
     #look at the top card of the deck without drawing
     def peek(self):
-        #TODO
-        return 0
+        return self.contents[0]
     #TODO implement other useful deck functions (couldn't think of everything, it's 2:15 in the morning)
