bca69ed8be1271b85c64be3f9eee10be16666172
Parker Swanson <pkswanson1@outlook.com>
2019-12-04 23:16:51 -0500

    added player class and shells of future helpful functions

--- dc_player.py
+++ dc_player.py
@@ -1,6 +1,20 @@
-#Contains helper functions for game.py
+#Contains helper functions for game.py and the Player class
 import deck
-import game
+import buy_cards
+import pygame
+
+"""
+Class for each player entity
+Created by dc_player.player(own_deck, isComputer)
+own_deck: the player's deck to start
+isComptuer: whether this is a computer or human player 
+"""
+class Player:
+
+    def __init__(self, own_deck, isComputer):
+        self.own_deck = own_deck
+        self.isComputer = isComputer
+
 """
 Sets up main deck.
 Parameter list: 
@@ -14,11 +28,12 @@ def load_main_deck(card_list):
 
 """
 Sets up a player deck.
+Parameter list:
+    card list: cards that are in the initial player deck
 Returns: loaded main deck
 """
-def load_player_deck():
+def load_player_deck(card_list):
     #TODO make subclass for player decks
-    card_list = [game.Punch]*7 + [game.Vulnerability]*3
     player_deck = deck.Deck(card_list)
     player_deck.shuffle()
     return player_deck
@@ -32,5 +47,27 @@ Returns: loaded villain deck
 def load_villain_deck(super_villains):
     villain_deck = deck.Deck(super_villains)
     villain_deck.shuffle()
-    villain_deck.add_to_front(game.The_Flash) #the Flash starts every game
     return villain_deck
+
+#remove card from lineup
+def remove_from_lineup(card):
+    pass
+
+#called when a card is bought
+def buy_card(player, card):
+    pass
+
+
+#run the computer turn
+def computer_turn(player, super_villain_deck, main_deck, kick_deck, lineup):
+    #TODO figure out power via the playing cards algorithm
+    power = 0
+    #list of cards to buy
+    cards_to_buy = buy_cards.buy_cards(power, super_villain_deck, main_deck, kick_deck, player.own_deck, lineup)
+    for card in cards_to_buy:
+        buy_card(player, card)
+    pass
+
+#loop to run while the player is taking their turn
+def player_turn():
+    pass
