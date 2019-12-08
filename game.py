c2ad42cd3ecb4b496a2b747bfd0f19cf16776f27
Dan Felley <dfelley@cylon.cs.oberlin.edu>
2019-12-08 13:25:45 -0500

    Made a mistake, fixed it now

--- game.py
+++ game.py
@@ -529,9 +529,6 @@ for i in range(5):
 for i in range(5):
     human_player.own_deck.draw()
 
-for i in range(20):
-    human_player.own_deck.hand.append(random.choice(StartingMainDeck))
-
 while not done:
     mouse_pos = pygame.mouse.get_pos() # assume we will always need to know the position of the mouse
 
