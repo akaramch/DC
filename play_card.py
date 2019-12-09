dcf63eee350bc893e919207b11bfe50a31ca8f33
Kabir Karamchandani <kabirkaramchandani@gmail.com>
2019-12-08 19:46:20 -0500

    Changed to adding kicks to copy

--- play_card.py
+++ play_card.py
@@ -57,3 +57,62 @@ def to_play(hand, own_deck):
             min = curr_priority
         i += 1
     return hand[index]
+"""
+Given the top card of your library, will return True if you should destroy it, false otherwise
+"""
+def top_destroy(card):
+    if card.name == "Weakness" or card.name == "Vulnerability" or card.name == "Punch":
+        return True
+    else:
+        return False
+"""
+Given your hand, will return the card you should destroy
+If you don't have a punch weakness, or vulnerability, it will destroy the card with least vp
+This for when you have to destroy, there's another for optional destruction.
+"""
+def must_hand_destroy(hand):
+    for card in hand:
+        if card.name == "Weakness":
+            return card
+    for card in hand:
+        if card.name == "Vulnerability":
+            return card
+    for card in hand:
+        if card.name == "Punch":
+            return card
+    min = 0
+    index = 11
+    i = 0 #iterator
+    for card in hand:
+        if card.vp > min:
+            min = card.vp
+            index = i
+        i += 1
+    return hand[index]
+"""
+Given your hand, will return the card you should destroy if it's optional 
+returns none if you shouldn't destroy any cards
+Currently doesn't have code to destroy punches only if it prevents you from buying super
+Need to look into whether it's worth passing this function enough things to add that code
+"""
+def can_hand_destroy(hand):
+    to_destroy = must_hand_destroy(hand)
+    if to_destroy.name == "Weakness" or to_destroy.name == "Vulnerability" or to_destroy.name == "Punch":
+        return to_destroy
+    else:
+        return None
+"""
+Given your discard pile, will return which card you should destroy
+Returns none if you shouldn's
+"""
+def discard_destroy(discard):
+    for card in discard:
+        if card.name == "Weakness":
+            return card
+    for card in discard:
+        if card.name == "Vulnerability":
+            return card
+    for card in discard:
+        if card.name == "Punch":
+            return card
+    return None
